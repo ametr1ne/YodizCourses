fe5c2c0c49e5148ac01dfeb66e793a2cbcb28d20
ametr1ne <nikon.filanovsky@gmail.com>
2022-04-06 17:38:41 +0400

    vimeo videos

--- prod/functions.php
+++ prod/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/lesson1.min.css');
+    wp_enqueue_style('style', get_template_directory_uri() . '/assets/scss/lesson1.css');
 });
 
 function selectingPage($post, $link)
@@ -61,18 +61,21 @@ function paidRandom()
 function showVideos()
 {
 
-    $listTheory = fillVideosArray();
+    $videoList = fillVideosArray();
 
-    if ($listTheory != null) {
+    if ($videoList != null) {
 
-        foreach ($listTheory as $index => $post_item) {
+        foreach ($videoList as $index => $post_item) {
 
-            $videoTitle = get_post_meta((int)SCF::get('theory')[0][$index])['video_title'];?>
+            $videoTitle = get_post_meta((int)SCF::get('videos')[0][$index])['video_title']; ?>
             <div class="theory content-item video-block">
                 <h2><?= $videoTitle[0] ?></h2>
-                <div class="video" data-videohref="<?= $post_item['video_link'] ?>">
-                    <div class="video_player" id="theory_player<?= $index ?>"></div>
+                <div class="video">
+                        <iframe src="https://player.vimeo.com/video/<?= $post_item['video_link'] ?>"
+                                frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen
+                                width="100%" height="100%"></iframe>
                 </div>
+
                 <ul>
                     <?
                     if ($post_item['timecodes']['name'][0] != null)
@@ -95,92 +98,31 @@ function showVideos()
             <?php
         }
     }
-
-    $listPractice = fillArrayPractice();
-
-    if ($listPractice != null) {
-
-        foreach ($listPractice as $index => $post_item) {
-            $videoTitle = get_post_meta((int)SCF::get('practice')[0][$index])['video_title'];?>
-            <div class="homework content-item video-block">
-                <h2><?= $videoTitle[0] ?></h2>
-                <div class="video" data-videohref="<?= $post_item['video_link'] ?>">
-                    <div class="video_player" id="practice_player<?= $index ?>"></div>
-                </div>
-                <ul>
-                    <?
-                    if ($post_item['timecodes']['name'][0] != null)
-                        for ($i = 0; $i < count($post_item['timecodes']['time']); $i++) { ?>
-                            <li>
-                                <a class="timecode timecode-theory"
-                                   data-timecode="<?= $post_item['timecodes']['time'][$i] ?>"
-                                   data-index="<?= $index ?>">
-                                    <svg width="8" height="8" viewBox="0 0 9 8" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <circle cx="4.71875" cy="4" r="4" fill="#D0D0E1"/>
-                                    </svg>
-                                    <?= $post_item['timecodes']['name'][$i] ?>
-                                </a>
-                            </li>
-                        <? } ?>
-                </ul>
-            </div>
-        <?php }
-    }
 }
 
-function fillArrayTheory()
+function fillVideosArray()
 {
 
-    $theory_timecodes = [];
+    $video_timecodes = [];
 
-    if (sizeof(SCF::get('theory')) > 0) {
+    if (sizeof(SCF::get('videos')) > 0) {
 
-        $theory = SCF::get('theory')[0];
+        $videos = SCF::get('videos')[0];
 
-        for ($i = 0; $i < sizeof($theory); $i++) {
+        for ($i = 0; $i < sizeof($videos); $i++) {
 
-            $full_meta = get_post_meta((int)$theory[$i]);
+            $full_meta = get_post_meta((int)$videos[$i]);
 
-            $theory_lessons = $full_meta['name'];
-            $theory_times = $full_meta['time'];
-            $theory_timecodes[$i]['video_link'] = $full_meta['video_link'][0];
+            $video_lessons = $full_meta['name'];
+            $video_times = $full_meta['time'];
+            $video_timecodes[$i]['video_link'] = $full_meta['video_link'][0];
 
-            for ($j = 0; $j < count($theory_lessons); $j++) {
-                $theory_timecodes[$i]['timecodes']['name'][$j] = $theory_lessons[$j];
-                $theory_timecodes[$i]['timecodes']['time'][$j] = $theory_times[$j];
+            for ($j = 0; $j < count($video_lessons); $j++) {
+                $video_timecodes[$i]['timecodes']['name'][$j] = $video_lessons[$j];
+                $video_timecodes[$i]['timecodes']['time'][$j] = $video_times[$j];
             }
         }
-        return $theory_timecodes;
-    }
-}
-
-function fillArrayPractice()
-{
-
-    $practice_timecodes = [];
-
-    if (sizeof(SCF::get('practice')) > 0) {
-
-        $practice = SCF::get('practice')[0];
-
-        if (sizeof($practice) > 0) {
-
-            for ($i = 0; $i < sizeof($practice); $i++) {
-
-                $full_meta = get_post_meta((int)$practice[$i]);
-
-                $practice_lessons = $full_meta['name'];
-                $practice_times = $full_meta['time'];
-                $practice_timecodes[$i]['video_link'] = $full_meta['video_link'][0];
-
-                for ($j = 0; $j < count($practice_lessons); $j++) {
-                    $practice_timecodes[$i]['timecodes']['name'][$j] = $practice_lessons[$j];
-                    $practice_timecodes[$i]['timecodes']['time'][$j] = $practice_times[$j];
-                }
-            }
-        }
-        return $practice_timecodes;
+        return $video_timecodes;
     }
 }
 
